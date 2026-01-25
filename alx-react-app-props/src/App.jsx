810ebd75f64523586846887f1ba59ff0f2d8e248
Ugochukwuka <your-email@domain.com>
2026-01-26 05:22:48 +1000

    Refactor prop drilling to Context API using UserContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,22 +1,15 @@
-import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
-
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 import UserContext from './UserContext';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const userData = {
     name: "Jane Doe",
-    email: "jane.doe@example.com",
+    email: "jane.doe@example.com"
   };
 
   return (
@@ -25,28 +18,10 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
-
         <WelcomeMessage />
 
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-        <UserProfile name="Bob" age="30" bio="Enjoys painting and traveling" />
-
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-
-        <h1>Vite + React</h1>
-
-        <div className="card">
-          <button onClick={() => setCount(count + 1)}>
-            count is {count}
-          </button>
-        </div>
+        {/* Context consumer */}
+        <UserProfile />
       </>
     </UserContext.Provider>
   );
