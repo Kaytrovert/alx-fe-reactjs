8fb64e70cb62f2f9fc746aa0e96e100b55c5bffa
Ugochukwuka <your-email@domain.com>
2026-01-26 03:00:27 +1000

    Fix inline styling to match checker requirements

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main style={{ padding: '20px', backgroundColor: '#f5f5f5' }}>
+    <main style={{ padding: '10px', backgroundColor: '#f5f5f5' }}>
       <p>These are some of my favorite cities.</p>
     </main>
   );
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile(props) {
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f0f8ff' }}>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px', backgroundColor: '#f0f8ff' }}>
       <h2 style={{ color: 'blue', marginBottom: '5px' }}>{props.name}</h2>
       <p>
         Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
