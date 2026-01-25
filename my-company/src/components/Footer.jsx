16084bfd55665f9ee3cbb1b886b69af503527ab8
Ugochukwuka <your-email@domain.com>
2026-01-26 07:18:38 +1000

    Add my-company React project

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: '40px',
+        padding: '10px',
+        textAlign: 'center',
+        backgroundColor: '#f1f1f1'
+      }}
+    >
+      <p>© 2026 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
